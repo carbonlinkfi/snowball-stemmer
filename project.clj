38cfc224581be478bd656138b00cfab756ec532e
Jussi Mononen <jussi.mononen@carbonlink.fi>
2022-05-26 12:37:37 +0300

    Update project and clojure version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject snowball-stemmer "0.1.0"
+(defproject snowball-stemmer "0.1.1"
   :description "Provides simple wrapper for tartarus.org stemmer"
   :url "https://github.com/weavejester/snowball-stemmer"
   :license {:name "The New BSD License"
             :url "http://opensource.org/licenses/bsd-license.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
